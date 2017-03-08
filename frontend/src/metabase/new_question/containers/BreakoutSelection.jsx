f85061470a722bc8ab6d1e14306bf222c71ed88d
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-03-07 18:25:33 -0800

    basic category breakdown

--- frontend/src/metabase/new_question/containers/BreakoutSelection.jsx
+++ frontend/src/metabase/new_question/containers/BreakoutSelection.jsx
@@ -5,22 +5,37 @@ import { connect } from 'react-redux';
 import Card from '../components/Card';
 
 import {
+    isDate,
+    isNumber,
+    isCategory,
+} from 'metabase/lib/schema_metadata';
+
+import {
     selectAndAdvance,
     selectMetricBreakout,
     setTip
 } from '../actions';
 
-const mapStateToProps = (state) => ({
-    title: state.newQuestion.currentStep.title,
-    breakouts: state.metadata.tables[state.newQuestion.card.dataset_query.query.source_table].fields_lookup,
-    tip: state.newQuestion.currentStep.tip
-})
+const mapStateToProps = (state) => {
+    const rawFields = state.metadata.tables[state.newQuestion.card.dataset_query.query.source_table].fields_lookup
+    const fields = Object.keys(rawFields).map(field => rawFields[field])
+    return {
+        title: state.newQuestion.currentStep.title,
+        breakouts: {
+            'date':     fields.filter(f => isDate(f)),
+            'number':   fields.filter(f => isNumber(f)),
+            'category':   fields.filter(f => isCategory(f)),
+        },
+        tip: state.newQuestion.currentStep.tip
+    }
+}
 
 const mapDispatchToProps = ({
     selectAndAdvance,
     selectMetricBreakout,
     setTip
 })
+
 
 @connect(mapStateToProps, mapDispatchToProps)
 class BreakoutSelection extends Component {
@@ -33,28 +48,37 @@ class BreakoutSelection extends Component {
         return (
             <div>
                 <h3>{ title }</h3>
-                <ol className={cxs({ display: 'flex', flexWrap: 'wrap' })}>
+                <ol>
                     { Object.keys(breakouts).map(breakout =>
                         <li
-                            onClick={() =>
-                                selectAndAdvance(() =>
-                                    selectMetricBreakout(breakouts[breakout])
-                                )
-                            }
-                            onMouseEnter={() => {
-                                if(breakouts[breakout].description) {
-                                    setTip({
-                                        title: breakouts[breakout].display_name,
-                                        text: breakouts[breakout].description
-                                    })
-                                }
-                                return false
-                            }}
-                            onMouseLeave={() => setTip(this.tip)}
-                            className={cxs({ flex: '0 0 33.33%', padding: '1em' })}
-                            key={breakouts[breakout].id}
+                            key={breakouts[breakout]}
                         >
-                            <Card name={ breakouts[breakout].display_name } />
+                            <h3>{breakout}</h3>
+                            <ol className={cxs({ display: 'flex', flexWrap: 'wrap' })}>
+                                { breakouts[breakout].map(f =>
+                                    <li
+                                        onClick={() =>
+                                            selectAndAdvance(() =>
+                                                selectMetricBreakout(f)
+                                            )
+                                        }
+                                        onMouseEnter={() => {
+                                            if(f.description) {
+                                                setTip({
+                                                    title: f.display_name,
+                                                    text: f.description
+                                                })
+                                            }
+                                            return false
+                                        }}
+                                        onMouseLeave={() => setTip(this.tip)}
+                                        className={cxs({ flex: '0 0 33.33%', padding: '1em' })}
+                                        key={f.id}
+                                    >
+                                        <Card name={ f.display_name } />
+                                    </li>
+                                )}
+                            </ol>
                         </li>
                     )}
                 </ol>
